496f0824043308ebba2e949568ff724b097d071d
Zeke Sikelianos <zeke@sikelianos.com>
2009-11-17 14:28:02 -0800

    Configured database settings for heroku

--- environment.rb
+++ environment.rb
@@ -21,7 +21,7 @@ configure do
                  :url_base => 'http://localhost:4567/'
                )
 
-  DataMapper.setup(:default, "sqlite3:///#{File.expand_path(File.dirname(__FILE__))}/#{Sinatra::Base.environment}.db")
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3:///#{File.expand_path(File.dirname(__FILE__))}/#{Sinatra::Base.environment}.db")
 
   # load models
   $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
